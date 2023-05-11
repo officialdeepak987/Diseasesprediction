0627e95a7355a951fb5d5f6815a7d8d02a1ac69e
Deepak Singh <deepak5032singh@gmail.com>
2023-05-11 18:21:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+// const hamburger=document.querySelector(".humburger");
+// hamburger.onClick=function(){
+//     var menu=document.querySelector(".nav-bar");
+//     menu.classList.toggle("active");
+// }
+
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".header");
+
+const toggleNavbar = () => {
+  
+  nav_header.classList.toggle("active");
+};
+
+mobile_nav.addEventListener("click", () => toggleNavbar());
+
+
+
+
+// aos
+
+
+AOS.init({
+  delay:0,
+  duration:900,
+  once:true
+})
